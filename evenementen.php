038ed47fb4d59afbc4a17bf09d3c255227e587cc
1043099 <95744484+1043099@users.noreply.github.com>
2022-06-27 10:31:45 +0200

    Update evenementen.php

--- evenementen.php
+++ evenementen.php
@@ -1,9 +1,5 @@
 <html>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="css/artiesten.css" rel="stylesheet" type="text/css">
+<link href="css/evenementen.css" rel="stylesheet" type="text/css">
 <nav class="navbar navbar-expand-sm navbar-dark">
     <img src="images/LOGO (2).png" width="200" alt=""> 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation"> 
