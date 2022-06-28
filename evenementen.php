a8975447eab59a064190e6767ea58e649755772e
1043099 <95744484+1043099@users.noreply.github.com>
2022-06-28 10:21:02 +0200

    Update evenementen.php

--- evenementen.php
+++ evenementen.php
@@ -1,72 +1,47 @@
-<html>
-<link href="css/evenementen.css" rel="stylesheet" type="text/css">
-<nav class="navbar navbar-expand-sm navbar-dark">
-    <img src="images/LOGO (2).png" width="200" alt=""> 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation"> 
-        <span class="navbar-toggler-icon"></span> 
-    </button>
-    <div class="end">
-        <div class="collapse navbar-collapse" id="navbarColor02">
-            <ul class="navbar-nav">
-            <li class="nav-item active"> <a class="nav-link mt-2" href="#" data-abc="true" id="clicked">Home</a> </li>
-                <li class="nav-item"> <a class="nav-link" href="product.php" data-abc="true">Producten</a> </li>
-                <li class="nav-item "> <a class="nav-link" href="#" data-abc="true">Evenementen</a> </li>
-                <li class="nav-item"> <a class="nav-link" href="#" data-abc="true">Contact</a> </li>
-				<li class="nav-item"> <a class="nav-link" href="#" data-abc="true">Inloggen</a> </li>
-            </ul>        
-        </div>
-    </div>    
-</nav>
-<!-- Main Body -->
-
-<?php
-
-?>
-
-<table>
-    <tr>
-        <th>Artiest</th>
-        <th>Naam</th>
-        <th>Statement</th>
-        <th>Telefoon nr.</th>
-        <th>Activiteit</th>
-    </tr>    
-    <?php
-    require('config.php');
-
-
-    $sql = "SELECT * FROM artiesten";
-    if($result = $conn-> query($sql)){
-        while($row = $result->fetch_assoc()) {
-            echo "<tr><td>";
-            echo $row['naam'];
-            echo "</td>";
-            echo "<td>";
-            echo $row['voornaam']." ".$row['tussenvoegsel']." ".$row['achternaam'];
-            echo "</td>";
-            echo "<td>";
-            echo $row['statement'];
-            echo "</td>";
-            echo "<td>";
-            echo $row['telefoon']; 
-            echo "</td>";
-            echo "<td>";
-            if($row['actief'] == 1){
-                echo "Actief";
-            }else if ($row['actief'] == 0){
-                echo "Non actief";
-            }
-            echo "</td></tr>";
-        }
-    }
-
-    ?>
-</table>
-
-<section>
-    <div class="container">
-        <div class="row">
-        </div>
-    </div>
-</section>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Victory Energy">
+  <meta name="author" content="Davey Derksen">
+  <meta name="keywords" content="">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="js/script.js"></script>
+  <title>Victory Energy</title>
+</head>
+<header>
+        <section class="navbar">
+        <nav class="nav">
+            <ul class="grid">
+                <div class="dropdown">
+                    <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+                    <div id="myDropdown" class="dropdown-content">
+                      <a href="index.php">Home</a>
+                      <a href="evenementen.php">Evenementen</a>
+                      <a href="informatie.php">Informatie</a>
+                      <a href="inloggen.php">Login</a>
+                    </div>
+                </div>
+                <li><a href="#" class="disapear2"><img src="images/hamburger.webp" width="80" height="50"></a></li>
+                <img src="images/LOGO (2).png" class="logo" alt="">
+                <li><a href="index.php" class="disapear">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home</a></li>
+                <li><a href="evenementen.php" class="disapear">&nbsp;&nbsp;&nbsp;&nbsp;Evenementen</a></li>
+                <li><a href="informatie.php" class="disapear">&nbsp;&nbsp;&nbsp;&nbsp;Informatie</a></li>
+                <li><a href="inloggen.php" class="disapear">Login</a></li>
+            </ul>
+        </nav>
+    </section>
+    </header>
+<body>
+                <section class="socialicons2">
+                    <a href=""><img src="images/twitter.png" class="socials"></a>
+                    <a href=""><img src="images/fb.png" class="socials"></a>
+                    <a href=""><img src="images/insta.png" class="socials"></a>
+                </section>
+            </article>
+        </section>
+    </main>
+</body>
 </html>
